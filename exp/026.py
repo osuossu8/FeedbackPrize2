7cb7e88c307d64f98f31377403040bb2114d4b2d
osuossu8 <osuosuossu18@gmail.com>
2022-08-04 13:29:20 +0000

    026

--- exp/026.py
+++ exp/026.py
@@ -90,7 +90,7 @@ class CFG:
     scheduler = 'cosine'
     batch_size = 8 # 16 # 8
     num_workers = 3
-    lr = 5e-6
+    lr = 8e-6 # 5e-6
     weigth_decay = 0.01
     epochs = 4 # 5 # 4
     n_fold = 4
